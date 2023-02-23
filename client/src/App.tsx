9e7d38bf205a4cd708a81af7fae205251feaa0ef
exellentcoin26 <jonas.caluwe26@gmail.com>
2023-02-23 08:43:44 +0100

    add base structure for react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './routes/Home';
+import Login from './routes/Login';
+import Register from './routes/Register';
+
 const App = () => {
-    return <p>Hello, World!</p>;
+    return (
+        <BrowserRouter>
+            <Navigation />
+            <Routes>
+                <Route path="/" element={<Navigate to="/home" replace />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
