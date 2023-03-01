aa10cb5c2f7c8a12643844a94e4810771cb2018d
Laurens De Wachter <laurens.dewachter@hotmail.com>
2023-03-01 18:01:39 +0100

    added about page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Home from './routes/Home';
 import Login from './routes/Login';
 import Register from './routes/Register';
 import Admin from './routes/Admin';
+import About from './routes/About';
 
 const App = () => {
     return (
@@ -15,6 +16,7 @@ const App = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/admin" element={<Admin />} />
+                <Route path="/about" element={<About />} />
             </Routes>
         </BrowserRouter>
     );
