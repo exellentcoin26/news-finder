38736f8e8f7ad7bde13360d587c13b03ecf90b40
Laurens De Wachter <laurens.dewachter@hotmail.com>
2023-03-01 11:08:59 +0100

    add non-functional add rss-feed button on admin page

--- client/src/routes/Admin.tsx
+++ client/src/routes/Admin.tsx
@@ -1,5 +1,12 @@
+import Button from 'react-bootstrap/Button';
+
 const Admin = () => {
-    return <p>Admin page</p>;
+    return (
+        <>
+            <p>Admin page</p>
+            <Button>Add RSS-Feed</Button>
+        </>
+    );
 };
 
 export default Admin;
