b3c4cfb709f9b0fd1e15ee5d20fcc52995f34b15
chloeacm <anne.mansibang@hotmail.com>
2023-03-01 10:42:58 +0100

    created login page

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -10,7 +10,7 @@ const Login = () => {
                 <div>
                     <label> Password </label>
                     <input type={"password"}/>
-                </div>
+                </div>  
             </form>
         </>
     )
