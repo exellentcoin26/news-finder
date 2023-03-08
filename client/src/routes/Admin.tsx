298d249a4a6c3c56e1d83b030d9ec9c915246da9
david s <davids@MacBook-Pro-de-david.local>
2023-03-08 18:43:21 +0100

    added color to buttons

--- client/src/routes/Admin.tsx
+++ client/src/routes/Admin.tsx
@@ -4,32 +4,28 @@ import '../../public/styles/Admin.css';
 const Admin = () => {
     return (
         <>
-            
+        <head>
         <p>Admin Page</p>
+        </head>
             <body>
                 <form>
                 <div>
                     <div>
-                        <label>RSS-feed</label><br/>
+                        <label>RSS-feed</label>
                         <input type='text'/><br/>
-                        <Button type='submit'>add</Button>
-                        <Button type='submit'>remove</Button>
+                        <button className='button button1'>add</button>
+                        <button className='button button2'>remove</button>
                     </div>
                     <div>
-                        <label>Xml-xpaths</label><br/>
+                        <label>Xml-xpaths</label>
                         <input type='text'/><br/>
-                        <Button type='submit'>add</Button>
+                        <button className='button button1'>add</button>
                     </div>  
                     <div>
-                        <label>Admin</label><br/>
+                        <label>Admin</label>
                         <input type='text'/><br/>
-                        <Button type='submit'>add</Button>                   
+                        <button className='button button1'>add</button>                   
                     </div>
-                    <div>
-                        <label>username</label>
-                        <input type='text'/> <br />
-                    </div>
-                    <Button>login</Button>
                 </div>            
             </form>
             </body>
