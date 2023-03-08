e57070d540c12c5611af6c6f0676cc77da18a5fd
david s <davids@mbp-de-david.uantwerpen.be>
2023-03-08 13:22:08 +0100

    Admin page

--- client/src/routes/Admin.tsx
+++ client/src/routes/Admin.tsx
@@ -3,10 +3,30 @@ import Button from 'react-bootstrap/Button';
 const Admin = () => {
     return (
         <>
-            <h1>Foo page</h1>
-
-            <p>Admin page</p>
-            <Button>Add RSS-Feed</Button>
+            
+        <p>Login Page</p>
+            <form>
+                <div>
+                    <label> Add RSS-feed</label><br/>
+                    <input type='text'/>
+                </div>
+                <div>
+                    <label> Xml xpaths</label><br/>
+                    <input type='text'/>
+                </div>  
+                <div>
+                    <label> Add Admin</label><br/>
+                    <input type='text'/>                    
+                </div>
+                <div>
+                    <label> Remove RSS-feed </label><br/>
+                    <input type='text'/><br/>
+                </div>
+                    <Button>submit</Button>
+            </form>
+        
+            
+            
         </>
     );
 };
