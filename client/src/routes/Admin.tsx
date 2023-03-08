34d90aeee3f5c3388daf6517104672a8235f132f
david s <davids@MacBook-Pro-de-david.local>
2023-03-08 16:53:07 +0100

    add username&password and Admin.css

--- client/src/routes/Admin.tsx
+++ client/src/routes/Admin.tsx
@@ -1,31 +1,42 @@
 import Button from 'react-bootstrap/Button';
+import '../../public/styles/Admin.css';
 
 const Admin = () => {
     return (
         <>
             
         <p>Admin Page</p>
-            <form>
+            <body>
+                <form>
                 <div>
-                    <label> RSS-feed</label><br/>
-                    <input type='text'/><br/>
-                    <Button type='submit'>add</Button>
-                    <Button type='submit'>remove</Button>
-                </div>
-                <div>
-                    <label> Xml xpaths</label><br/>
-                    <input type='text'/><br/>
-                    <Button type='submit'>add</Button>
-                </div>  
-                <div>
-                    <label>Admin</label><br/>
-                    <input type='text'/><br/>
-                    <Button type='submit'>add</Button>                   
+                    <div>
+                        <label>RSS-feed</label><br/>
+                        <input type='text'/><br/>
+                        <Button type='submit'>add</Button>
+                        <Button type='submit'>remove</Button>
+                    </div>
+                    <div>
+                        <label>Xml-xpaths</label><br/>
+                        <input type='text'/><br/>
+                        <Button type='submit'>add</Button>
+                    </div>  
+                    <div>
+                        <label>Admin</label><br/>
+                        <input type='text'/><br/>
+                        <Button type='submit'>add</Button>                   
+                    </div>
+                    <div>
+                        <label>username</label>
+                        <input type='text'/> <br />
+                    </div>
+                    <div>
+                        <label>password</label>
+                        <input type='text' /> <br />
+                        <Button>login</Button>
+                    </div>
                 </div>            
             </form>
-        
-            
-            
+            </body>
         </>
     );
 };
