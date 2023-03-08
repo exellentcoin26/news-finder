2ff780a02f15dee9616241e8786460c4a2eb3c66
david s <davids@mbp-de-david.uantwerpen.be>
2023-03-08 11:41:14 +0100

    Test commit

--- client/src/routes/Admin.tsx
+++ client/src/routes/Admin.tsx
@@ -3,6 +3,8 @@ import Button from 'react-bootstrap/Button';
 const Admin = () => {
     return (
         <>
+            <h1>Foo page</h1>
+
             <p>Admin page</p>
             <Button>Add RSS-Feed</Button>
         </>
