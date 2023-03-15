ffbdc29e0a083a4c8a6a3dfc69e470d29c308b3d
exellentcoin26 <jonas.caluwe26@gmail.com>
2023-03-15 16:34:49 +0100

    Fix extracting photos from the Rss 2.0 format

--- rss-scraper/src/main.rs
+++ rss-scraper/src/main.rs
@@ -44,7 +44,9 @@ async fn scrape_rss_feeds(client: &PrismaClient) -> Result<()> {
                 // is invalid.
                 // TODO: Should I remove the feed if it was invalid? It is possible that the feed
                 // was reachable at this time, but is completely valid.
-                eprintln!("Rss feed invalid or (temporarily) not reachable");
+                eprintln!(
+                    "Rss feed invalid or (temporarily) not reachable (rss feed: `{rss_feed}`)"
+                );
                 continue;
             }
         };
@@ -87,29 +89,23 @@ async fn scrape_rss_feeds(client: &PrismaClient) -> Result<()> {
             };
 
             let title = match entry.title {
-                Some(title) => title.content,
+                Some(ref title) => title.content.clone(),
                 None => {
                     eprintln!("Article entry has no title (rss feed: `{rss_feed}`)");
                     continue;
                 }
             };
 
-            // TODO: Fix photo's for some rss feeds
-
             let description = entry
                 .summary
+                .as_ref()
                 .map(|description| description.content.to_string());
 
-            let photo = entry
-                .links
-                .iter()
-                .filter(|link| {
-                    link.media_type.as_ref().map_or(false, |media_type| {
-                        media_type == "image/jpeg" || media_type == "image/png"
-                    })
-                })
-                .next()
-                .map(|link| link.href.to_string());
+            // TODO: Fix photo's for some rss feeds
+            // For Rss 2.0, photos are located in the enclosure tag. Which is located in the
+            // `entry::content::src::..` tag.
+
+            let photo = get_photos_from_entry(&entry).into_iter().next();
 
             let pub_date: Option<chrono::DateTime<chrono::FixedOffset>> =
                 entry.published.map(|date| date.into());
@@ -120,10 +116,11 @@ async fn scrape_rss_feeds(client: &PrismaClient) -> Result<()> {
                 .map(|category| category.term.clone())
                 .collect();
 
-            println!("title: `{title}`, url: `{url}`, photo: `{photo:?}`, description: {description:?}, pub_date: `{pub_date:?}`");
+            println!("title: `{title}`, url: `{url}`, photo: `{photo:?}`, description: {description:?}, pub_date: `{pub_date:?}`, tags: {labels:?}");
 
             /* insert scraped data into db */
 
+            // TODO: Make labels work :)
             // TODO: Connect labels to articles.
 
             // insert labels
@@ -142,15 +139,15 @@ async fn scrape_rss_feeds(client: &PrismaClient) -> Result<()> {
                     prisma::news_sources::id::equals(source_id),
                     url,
                     title,
-                    vec![],
+                    vec![
+                        prisma::news_articles::description::set(description),
+                        prisma::news_articles::photo::set(photo),
+                        prisma::news_articles::publication_date::set(pub_date),
+                    ],
                 ),
                 // Update is not needed, because we want to ignore articles that have been inserted
                 // before.
-                vec![
-                    prisma::news_articles::description::set(description),
-                    prisma::news_articles::photo::set(photo),
-                    prisma::news_articles::publication_date::set(pub_date),
-                ],
+                vec![],
             ));
         }
 
@@ -233,4 +230,46 @@ async fn get_rss_and_validate(url: &str) -> std::result::Result<Feed, RssError> 
     let feed = feed_rs::parser::parse(&content[..])?;
 
     Ok(feed)
+}
+
+/// Returns a vector of photo urls found in the entry.
+/// Locations:
+///     * Atom: entry::links with media_type equal to `image/jpeg`.
+///     * Rss 2.0: entry::content::src with type equal to `image/jpeg`.
+fn get_photos_from_entry(entry: &feed_rs::model::Entry) -> Vec<String> {
+    let mut photos: Vec<String> = entry
+        .links
+        .iter()
+        // Atom format
+        .filter(|&link| {
+            link.media_type.as_ref().map_or(false, |media_type| {
+                media_type == "image/jpeg" || media_type == "image/png"
+            })
+        })
+        .map(|link| link.href.clone())
+        .collect();
+
+    // Rss 2.0 format
+    for media_entry in &entry.media {
+        let rss2_photos: Vec<String> = media_entry
+            .content
+            .iter()
+            .filter(|content| {
+                content
+                    .content_type
+                    .as_ref()
+                    .map_or(true, |content_type| content_type.type_() == mime::IMAGE)
+            })
+            .filter_map(|content| {
+                content
+                    .url
+                    .as_ref()
+                    .map_or(None, |url| Some(url.to_string()))
+            })
+            .collect();
+
+        photos.extend(rss2_photos);
+    }
+
+    photos
 }
