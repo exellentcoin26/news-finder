067f877612f74305b84b0a8dcfd8ef0368562192
ayoubelmarch <ayoub.elmarchouhi@hotmail.com>
2023-03-19 13:42:36 +0100

    Add delete rss route

--- server/news_finder/routes/rss.py
+++ server/news_finder/routes/rss.py
@@ -2,7 +2,7 @@ from flask import Blueprint, Response, request, make_response
 from urllib.parse import ParseResult, urlparse
 from http import HTTPStatus
 from jsonschema import SchemaError, validate, ValidationError
-from prisma.errors import UniqueViolationError
+from prisma.errors import UniqueViolationError, RecordNotFoundError
 
 from news_finder.db import get_db
 from news_finder.utils.error_response import make_error_response, ResponseError
@@ -26,6 +26,23 @@ schema = {
     "required": ["feeds"]
 }
 
+def data_validation(data):
+    if not data:
+        return make_error_response(
+            ResponseError.InvalidJson, "", HTTPStatus.BAD_REQUEST
+        )
+
+    try:
+        validate(instance=data, schema=schema)
+    except ValidationError as e:
+        return make_error_response(
+            ResponseError.JsonValidationError, e.message,
+            HTTPStatus.BAD_REQUEST
+        )
+    except SchemaError as e:
+        print(f"jsonschema is invalid: {e.message}", file=sys.stderr)
+        raise e
+
 
 @rss_bp.post("/")
 async def add_rss_feed() -> Response:
@@ -43,21 +60,7 @@ async def add_rss_feed() -> Response:
     """
 
     data = request.get_json(silent=True)
-    if not data:
-        return make_error_response(
-            ResponseError.InvalidJson, "", HTTPStatus.BAD_REQUEST
-        )
-
-    try:
-        validate(instance=data, schema=schema)
-    except ValidationError as e:
-        return make_error_response(
-            ResponseError.JsonValidationError, e.message,
-            HTTPStatus.BAD_REQUEST
-        )
-    except SchemaError as e:
-        print(f"jsonschema is invalid: {e.message}", file=sys.stderr)
-        raise e
+    data_validation(data)
 
     db = await get_db()
 
@@ -102,6 +105,38 @@ async def add_rss_feed() -> Response:
     except UniqueViolationError as e:
         return make_error_response(
             ResponseError.UniqueViolationError,
+            str(e.with_traceback(None)), HTTPStatus.BAD_REQUEST
+        )
+    except Exception as e:
+        print(e.with_traceback(None), file=sys.stderr)
+        return make_error_response(
+            ResponseError.ServerError, "",
+            HTTPStatus.INTERNAL_SERVER_ERROR
+        )
+
+    return make_response("", HTTPStatus.OK)
+
+@rss_bp.delete("/")
+async def delete_rss() -> Response:
+    data = request.get_json(silent=True)
+    data_validation(data)
+
+    db = await get_db()
+
+    b = db.batch_()
+
+    for rss_feed in data["feeds"]:
+        b.rssentries.delete(
+            where={
+                "name": rss_feed
+            }
+        )
+
+    try:
+        await b.commit()
+    except RecordNotFoundError as e:
+        return make_error_response(
+            ResponseError.RecordNotFoundError,
             str(e.with_traceback(None)), HTTPStatus.BAD_REQUEST
         )
     except Exception as e:
