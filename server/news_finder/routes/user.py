e618ff92d9873a352be1b4426ea7382cefd25229
chloeacm <anne.mansibang@hotmail.com>
2023-03-21 16:53:10 +0100

    Implement login and register routes

--- server/news_finder/routes/user.py
+++ server/news_finder/routes/user.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, Response
+from flask import Blueprint, Response, request, make_response
+from prisma import Prisma
+from prisma.errors import UniqueViolationError 
+
+from ..db import get_db
+import secrets
 
 user_bp = Blueprint("user", __name__, url_prefix="/user")
 
@@ -6,3 +11,66 @@ user_bp = Blueprint("user", __name__, url_prefix="/user")
 @user_bp.get("/")
 def hello_user() -> Response:
     return Response("Hello, User!")
+
+
+async def create_cookie_for_user(prisma: Prisma, user_id: int) -> str:
+    user_cookie: str = secrets.token_hex(32)
+
+    await prisma.usercookies.create(data={
+        "cookie": user_cookie,
+        "user_id": user_id
+    })
+
+    return user_cookie
+
+
+@user_bp.post("/register")
+async def register_user() -> Response:
+    prisma = await get_db()
+    data = request.get_json()
+    username: str = data["username"]
+    password: str = data["password"]
+
+    user = await prisma.users.find_first(
+        where={"username": username}
+    )
+
+    if user is not None:
+        return make_response("", 409)
+
+    user = await prisma.users.create(data={
+        "username": username
+    })
+
+    await prisma.userlogins.create(data={
+        "password": password,
+        "id": user.id
+    })
+
+    cookie = None
+    while cookie is None:
+        try:
+            cookie = await create_cookie_for_user(prisma, user.id)
+        except UniqueViolationError:
+            pass
+
+    resp: Response
+    resp = make_response("", 200)
+    resp.set_cookie("session", cookie)
+
+    return resp
+
+
+@user_bp.post("/logout")
+async def logout_user() -> Response:
+    prisma = await get_db()
+    cookie: str = request.cookies.get("session")
+
+    await prisma.usercookies.delete(
+        where={
+            "cookie": cookie
+        }
+    )
+
+
+    return make_response("", 200)
