6cd87a03378c9f740839d92adb650e0c71bec1fc
exellentcoin26 <jonas.caluwe26@gmail.com>
2023-04-04 17:06:16 +0200

    Restyle articles to mockups

--- client/src/components/Article.tsx
+++ client/src/components/Article.tsx
@@ -1,29 +1,33 @@
-import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
-import React from 'react';
+import { Card, Row, Col, Container } from 'react-bootstrap';
 import '../styles/Article.css';
-import { Link } from 'react-router-dom';
 
-const Article: React.FC<{ title: string; img_src: string; alt: string }> = ({
+const Article = ({
     title,
     img_src,
-    alt,
+    description,
+}: {
+    title: string;
+    img_src: string;
+    description: string;
 }) => {
     return (
-        <Container className="article-container p-0">
-            <Link to="/" className="article-link">
-                <Container className="image-container p-0">
-                    <Image
+        <Card style={{ overflow: 'hidden' }} className={'article-card'}>
+            <Row md={1} className={'h-100'}>
+                <Col className={'article-image'}>
+                    <Card.Img
                         src={img_src}
-                        alt={alt}
-                        className="article-img img-fluid justify-content-center"
+                        className={'h-100'}
+                        style={{ objectFit: 'cover' }}
                     />
-                </Container>
-                <Container className="text-container py-1 px-2">
-                    {title}
-                </Container>
-            </Link>
-        </Container>
+                </Col>
+                <Col className={'h-100 '}>
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        <Card.Text>{description}</Card.Text>
+                    </Card.Body>
+                </Col>
+            </Row>
+        </Card>
     );
 };
 
--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -7,26 +7,30 @@ import '../styles/Home.css';
 const Home = () => {
     return (
         <Container className="home-container">
-            <Row>
+            <Row className="home-article">
                 <Col>
                     <Article
                         title="Local Cat Elected Mayor of Small Town"
                         img_src="/images/cat.jpg"
-                        alt="Cat elected mayor"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                     />
                 </Col>
+            </Row>
+            <Row className="home-article">
                 <Col>
                     <Article
                         title="Local Cat Elected Mayor of Small Town"
                         img_src="/images/cat.jpg"
-                        alt="Cat elected mayor"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem fringilla ut morbi tincidunt. Faucibus interdum posuere lorem ipsum dolor. Pellentesque dignissim enim sit amet venenatis urna. Velit laoreet id donec ultrices tincidunt arcu non sodales. Amet justo donec enim diam vulputate ut pharetra sit. Odio facilisis mauris sit amet massa vitae."
                     />
                 </Col>
+            </Row>
+            <Row className="home-article">
                 <Col>
                     <Article
                         title="Local Cat Elected Mayor of Small Town"
                         img_src="/images/cat.jpg"
-                        alt="Cat elected mayor"
+                        description="Foo bar baz qux quux corge grault garlpy waldo fred plugh xyzzy thud"
                     />
                 </Col>
             </Row>
