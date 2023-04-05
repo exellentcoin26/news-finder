78f6c48303af41e787fc52f44aea2beec990c746
exellentcoin26 <jonas.caluwe26@gmail.com>
2023-04-05 20:50:27 +0200

    Convert scraper to cli application

--- rss-scraper/src/main.rs
+++ rss-scraper/src/main.rs
@@ -1,28 +1,91 @@
-use crate::{prelude::*, prisma::PrismaClient};
+use crate::{prelude::*, scraper::scrape_rss_feeds};
+use clap::{error::ErrorKind, Args, CommandFactory, Parser, Subcommand};
 use dotenv::dotenv;
-use error::RssError;
-use feed_rs::model::Feed;
 use job_scheduler_ng::{Job, JobScheduler};
 use std::{
     fs,
     io::{BufRead, BufReader, BufWriter, Write},
+    path::PathBuf,
     time::Duration,
 };
 
 mod error;
 mod prelude;
 mod prisma;
+mod scraper;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about=None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Check RSS feeds validity from input file
+    Check {
+        /// File to read feeds from
+        #[arg(short, long, value_name = "INPUT_FILE", default_value = "input.txt")]
+        input: PathBuf,
+
+        /// File to write successful feeds to
+        #[arg(short, long, value_name = "OUTPUT_FILE", default_value = "working.txt")]
+        output: PathBuf,
+    },
+    /// Run the rss-scraper
+    Run {
+        #[command(flatten)]
+        args: ScrapeArgs,
+    },
+}
+
+#[derive(Args)]
+#[group(multiple = false)]
+struct ScrapeArgs {
+    /// Run once
+    #[arg(long)]
+    once: bool,
+
+    /// Run on the given interval in minutes
+    #[arg(short, long, value_name = "INTERVAL", default_value_t = 10)]
+    interval: u32,
+}
 
 #[tokio::main]
-async fn main() -> ! {
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+
     // load environment variables from `.env` file if present
     dotenv().ok();
 
+    match cli.command {
+        Commands::Check { input, output } => {
+            if !input.exists() {
+                let mut cmd = Cli::command();
+                cmd.error(ErrorKind::Io, "Input file does not exist").exit();
+            }
+
+            check_rss_feeds_from_file(&input, &output).await?;
+        }
+        Commands::Run { args } => {
+            if args.once {
+                run_scraper().await;
+            } else {
+                start_scrape_job(args.interval);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn start_scrape_job(interval: u32) -> ! {
     let mut schedule = JobScheduler::new();
 
     let mut job = Job::new(
         // run every 10 minutes
-        "* 1/10 * * * * *"
+        format!("* 1/{} * * * * *", interval)
             .parse()
             .expect("failed to parse cron job time schedule"),
         || {
@@ -53,141 +116,14 @@ async fn run_scraper() {
 
     scrape_rss_feeds(&client).await.unwrap();
 
-    println!("Successfully scraped rss feeds!")
-
-    // check_rss_feeds_from_file("rss-feeds.txt", "working.txt")
-    //     .await
-    //     .unwrap();
-}
-
-async fn scrape_rss_feeds(client: &PrismaClient) -> Result<()> {
-    let rss_feeds = client.rss_entries().find_many(vec![]).exec().await?;
-
-    for rss_feed in rss_feeds {
-        let source_id = rss_feed.source_id;
-        let rss_feed = rss_feed.feed;
-
-        let mut label_batch = Vec::new();
-        let mut article_batch = Vec::new();
-
-        let feed = match get_rss_and_validate(&rss_feed).await {
-            Ok(feed) => feed,
-            Err(_) => {
-                // An error occurred where either `reqwest` failed to fetch the url or the rss feed
-                // is invalid.
-                // TODO: Should I remove the feed if it was invalid? It is possible that the feed
-                // was reachable at this time, but is completely valid.
-                eprintln!(
-                    "Rss feed invalid or (temporarily) not reachable (rss feed: `{rss_feed}`)"
-                );
-                continue;
-            }
-        };
-
-        // TODO: Store the language of the article for equality checking
-
-        /* scrape the rss feed */
-
-        for entry in feed.entries {
-            /*
-                Required fields:
-                    * Source id
-                    * Url
-                    * Title
-
-                Optional fields:
-                    * Photo
-                    * Description
-                    * Publication date
-                    * Labels
-            */
-
-            let url = match entry
-                .links
-                .iter()
-                .filter(|link| {
-                    if let Some(ref media_type) = link.media_type {
-                        return media_type == "text/html";
-                    }
-
-                    true
-                })
-                .next()
-            {
-                Some(link) => link.href.to_string(),
-                None => {
-                    eprintln!("Article entry has no link to the article (rss feed: `{rss_feed}`)");
-                    continue;
-                }
-            };
-
-            let title = match entry.title {
-                Some(ref title) => title.content.clone(),
-                None => {
-                    eprintln!("Article entry has no title (rss feed: `{rss_feed}`)");
-                    continue;
-                }
-            };
-
-            let description = entry
-                .summary
-                .as_ref()
-                .map(|description| description.content.to_string());
-
-            let photo = get_photos_from_entry(&entry).into_iter().next();
-
-            let pub_date: Option<chrono::DateTime<chrono::FixedOffset>> =
-                entry.published.map(|date| date.into());
-
-            let labels: Vec<String> = entry
-                .categories
-                .iter()
-                .map(|category| category.term.clone())
-                .collect();
-
-            // println!("title: `{title}`, url: `{url}`, photo: `{photo:?}`, description: {description:?}, pub_date: `{pub_date:?}`, tags: {labels:?}");
-
-            /* insert scraped data into db */
-
-            // TODO: Make labels work :)
-            // TODO: Connect labels to articles.
-
-            // insert labels
-            for label in labels {
-                label_batch.push(client.labels().upsert(
-                    prisma::labels::name::equals(label.clone()),
-                    prisma::labels::create(label, vec![]),
-                    vec![],
-                ))
-            }
-
-            // insert articles
-            article_batch.push(client.news_articles().upsert(
-                prisma::news_articles::url::equals(url.clone()),
-                prisma::news_articles::create(
-                    prisma::news_sources::id::equals(source_id),
-                    url,
-                    title,
-                    vec![
-                        prisma::news_articles::description::set(description),
-                        prisma::news_articles::photo::set(photo),
-                        prisma::news_articles::publication_date::set(pub_date),
-                    ],
-                ),
-                // Update is not needed, because we want to ignore articles that have been inserted
-                // before.
-                vec![],
-            ));
-        }
-
-        client._batch((label_batch, article_batch)).await?;
-    }
-
-    Ok(())
+    println!("Successfully scraped rss feeds!");
 }
 
 #[allow(dead_code)]
-async fn check_rss_feeds_from_file(input_file_name: &str, output_file_name: &str) -> Result<()> {
+async fn check_rss_feeds_from_file(
+    input_file_name: &PathBuf,
+    output_file_name: &PathBuf,
+) -> Result<()> {
     let input_file = fs::File::open(input_file_name)?;
     let mut output_file = fs::File::create(output_file_name)?;
 
@@ -252,53 +188,4 @@ async fn check_rss_feeds_from_file(input_file_name: &str, output_file_name: &str
     output.flush()?;
 
     Ok(())
-}
-
-async fn get_rss_and_validate(url: &str) -> std::result::Result<Feed, RssError> {
-    let content = reqwest::get(url).await?.bytes().await?;
-    let feed = feed_rs::parser::parse(&content[..])?;
-
-    Ok(feed)
-}
-
-/// Returns a vector of photo urls found in the entry.
-/// Locations:
-///     * Atom: entry::links with media_type equal to `image/jpeg`.
-///     * Rss 2.0: entry::content::src with type equal to `image/jpeg`.
-fn get_photos_from_entry(entry: &feed_rs::model::Entry) -> Vec<String> {
-    let mut photos: Vec<String> = entry
-        .links
-        .iter()
-        // Atom format
-        .filter(|&link| {
-            link.media_type.as_ref().map_or(false, |media_type| {
-                media_type == "image/jpeg" || media_type == "image/png"
-            })
-        })
-        .map(|link| link.href.clone())
-        .collect();
-
-    // Rss 2.0 format
-    for media_entry in &entry.media {
-        let rss2_photos: Vec<String> = media_entry
-            .content
-            .iter()
-            .filter(|content| {
-                content
-                    .content_type
-                    .as_ref()
-                    .map_or(true, |content_type| content_type.type_() == mime::IMAGE)
-            })
-            .filter_map(|content| {
-                content
-                    .url
-                    .as_ref()
-                    .map_or(None, |url| Some(url.to_string()))
-            })
-            .collect();
-
-        photos.extend(rss2_photos);
-    }
-
-    photos
 }
