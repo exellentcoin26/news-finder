c1aa0bdbd7459313564699f841c1bbb4cc13c68f
exellentcoin26 <jonas.caluwe26@gmail.com>
2023-04-05 12:27:56 +0200

    Refactor

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -27,10 +27,10 @@ const Home = () => {
         const fetchArticles = async () => {
             try {
                 const articles = await getArticlesFromServer();
-                console.log(articles);
+                // console.log(articles);
                 setArticles(articles);
             } catch (error) {
-                console.log(error);
+                console.error(error);
             } finally {
                 setIsLoading(false);
             }
@@ -66,33 +66,6 @@ const Home = () => {
                     );
                 })
             )}
-            {/*<Row className="home-article">*/}
-            {/*    <Col>*/}
-            {/*        <Article*/}
-            {/*            title="Local Cat Elected Mayor of Small Town"*/}
-            {/*            img_src="/images/cat.jpg"*/}
-            {/*            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."*/}
-            {/*        />*/}
-            {/*    </Col>*/}
-            {/*</Row>*/}
-            {/*<Row className="home-article">*/}
-            {/*    <Col>*/}
-            {/*        <Article*/}
-            {/*            title="Local Cat Elected Mayor of Small Town"*/}
-            {/*            img_src="/images/cat.jpg"*/}
-            {/*            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem fringilla ut morbi tincidunt. Faucibus interdum posuere lorem ipsum dolor. Pellentesque dignissim enim sit amet venenatis urna. Velit laoreet id donec ultrices tincidunt arcu non sodales. Amet justo donec enim diam vulputate ut pharetra sit. Odio facilisis mauris sit amet massa vitae."*/}
-            {/*        />*/}
-            {/*    </Col>*/}
-            {/*</Row>*/}
-            {/*<Row className="home-article">*/}
-            {/*    <Col>*/}
-            {/*        <Article*/}
-            {/*            title="Local Cat Elected Mayor of Small Town"*/}
-            {/*            img_src="/images/cat.jpg"*/}
-            {/*            description="Foo bar baz qux quux corge grault garlpy waldo fred plugh xyzzy thud"*/}
-            {/*        />*/}
-            {/*    </Col>*/}
-            {/*</Row>*/}
         </Container>
     );
 };
