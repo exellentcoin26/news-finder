d549fb2a95a41da1175f248b122b16899c96ef76
chloeacm <anne.mansibang@hotmail.com>
2023-04-05 11:25:02 +0200

    typo

--- server/news_finder/routes/user.py
+++ server/news_finder/routes/user.py
@@ -13,7 +13,6 @@ from news_finder.utils.error_response import make_error_response, ResponseError
 
 user_bp = Blueprint("user", __name__, url_prefix="/user")
 
-
 @user_bp.get("/")
 def hello_user() -> Response:
     return Response("Hello, User!")
