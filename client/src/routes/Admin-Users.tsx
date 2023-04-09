0992471246c5887f100631fba958d68285a4e1d2
Laurens De Wachter <laurens.dewachter@hotmail.com>
2023-04-09 11:48:22 +0200

    Basic user management layout no interaction

--- client/src/routes/Admin-Users.tsx
+++ client/src/routes/Admin-Users.tsx
@@ -1,7 +1,31 @@
 import '../styles/Admin.css';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const Admin_Users = () => {
-    return <></>;
+    return (
+        <Container>
+            <Container className="page-title">User Management</Container>
+            <Container>
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Remove Users</Form.Label>
+                        <Form.Control placeholder="usernames"></Form.Control>
+                    </Form.Group>
+                    <Button type="submit">Submit</Button>
+                </Form>
+                <Container className="mb-5"></Container>
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Make Admin</Form.Label>
+                        <Form.Control placeholder="usernames"></Form.Control>
+                    </Form.Group>
+                    <Button type="submit">Submit</Button>
+                </Form>
+            </Container>
+        </Container>
+    );
 };
 
 export default Admin_Users;
