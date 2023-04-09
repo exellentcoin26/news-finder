6c09d45d4f4a8a037a4221054bd79a1c7f163433
David Scalais <david.scalais@gmail.com>
2023-04-09 23:23:20 +0200

    about route filled

--- client/src/routes/About.tsx
+++ client/src/routes/About.tsx
@@ -1,5 +1,49 @@
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 const About = () => {
-    return <p>About page</p>;
+    return (
+        <div className="about-us-container">
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>Our Mission</h1>
+                        <p>
+                            Our news aggregator collects and displays news articles from a variety of news sources, providing users with a centralized website to stay up-to-date with
+                            current events.
+                        </p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12}>
+                        <h2>Meet the Team</h2>
+                    </Col>
+                   <Col>
+                        <p>Jonas Caluwé (2nd Bachelor Computer Science)</p>
+                        <p>David Scalais (2nd Bachelor Computer Science)</p>
+                        <p>Laurens De Wachter (2nd Bachelor Computer Science)</p>
+                        <p>Chloe Mansibang (2nd Bachelor Computer Science)</p>
+                        <p>Ayoub El Marchouchi (2nd Bachelor Computer Science)</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12}>
+                    <h2>Functionality</h2>
+                        <ul>
+                        <li>News articles are pulled from a variety of sources and displayed in a single location</li>
+                        <li>Users can filter articles by category</li>
+                        <li>Article summaries and images are displayed for easy browsing</li>
+                        <li>Users can click on individual articles to read the full story on the source website</li>
+                        <li>Articles are updated in real-time as new content becomes available</li>
+                        <li>Users gets articles recommended by a recommendation system</li>
+                        <li>For a better view similar articles are also group together</li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
+            </div>
+
+    )
 };
 
 export default About;
