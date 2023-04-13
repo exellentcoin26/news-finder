bc3c05fe00968a5068a10c0a45a2f5174e723f01
Laurens De Wachter <laurens.dewachter@hotmail.com>
2023-04-13 15:18:05 +0200

    User deletion no visual confirmation

--- client/src/routes/Admin-Users.tsx
+++ client/src/routes/Admin-Users.tsx
@@ -1,9 +1,27 @@
 import Container from 'react-bootstrap/Container';
+import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import '../styles/Admin.css';
 
 const Admin_Users = () => {
+    const server_url =
+        import.meta.env['VITE_SERVER_URL'] || 'http://localhost:5000';
+    const target_url = server_url + '/user/';
+
+    const handleUserDeletion = async (username: string): Promise<boolean> => {
+        const response = await fetch(target_url, {
+            method: 'DELETE',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ username: username }),
+        });
+
+        return response.status == 200;
+    };
+
+    const [usernameDelete, setUsernameDelete] = useState('');
+    const [usernameMakeAdmin, setUsernameMakeAdmin] = useState('');
+
     return (
         <Container>
             <Container className="page-title">User Management</Container>
@@ -11,9 +29,20 @@ const Admin_Users = () => {
                 <Form>
                     <Form.Group className="mb-3">
                         <Form.Label>Remove Users</Form.Label>
-                        <Form.Control placeholder="usernames"></Form.Control>
+                        <Form.Control
+                            type="text"
+                            placeholder="username"
+                            value={usernameDelete}
+                            onChange={(event) =>
+                                setUsernameDelete(event.target.value)
+                            }
+                        ></Form.Control>
                     </Form.Group>
-                    <Button type="submit" variant="custom">
+                    <Button
+                        type="submit"
+                        variant="custom"
+                        onClick={() => handleUserDeletion(usernameDelete)}
+                    >
                         Remove
                     </Button>
                 </Form>
@@ -21,7 +50,14 @@ const Admin_Users = () => {
                 <Form>
                     <Form.Group className="mb-3">
                         <Form.Label>Make Admin</Form.Label>
-                        <Form.Control placeholder="usernames"></Form.Control>
+                        <Form.Control
+                            type="text"
+                            placeholder="usernames"
+                            value={usernameMakeAdmin}
+                            onChange={(event) =>
+                                setUsernameMakeAdmin(event.target.value)
+                            }
+                        ></Form.Control>
                     </Form.Group>
                     <Button type="submit" variant="custom">
                         Submit
