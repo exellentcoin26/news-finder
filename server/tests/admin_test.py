c5bcbc0aa9b7ce23f4d91ffeea278b189118a309
Laurens De Wachter <laurens.dewachter@hotmail.com>
2023-04-15 22:40:53 +0200

    Hide admin bar for non admins

--- server/tests/admin_test.py
+++ server/tests/admin_test.py
@@ -7,23 +7,13 @@ from http import HTTPStatus
 def test_add_admin(client: Flask.testing):
     response = client.post("/user/", json={"username": "jan", "password": "qwerty"})
     assert response.status_code == HTTPStatus.OK
-    response = client.post("/user/", json={"username": "piet-joris", "password": "qwerty"})
-    assert response.status_code == HTTPStatus.OK
-    response = client.post("/user/", json={"username": "korneel", "password": "qwerty"})
-    assert response.status_code == HTTPStatus.OK
 
-    response_make_admin = client.post("/admin/", json={"usernames": ["Jan", "piet-joris"]})
+    response_make_admin = client.post("/admin/", json={"usernames": ["Jan"]})
     assert response_make_admin.status_code == HTTPStatus.OK
 
-    response = client.post("/admin/status/", json={"username": "Jan"})
+    response = client.get("/admin/")
     assert response.status_code == HTTPStatus.OK
     assert response.get_json()["admin"]
-    response = client.post("/admin/status/", json={"username": "PIET-JORIS"})
-    assert response.status_code == HTTPStatus.OK
-    assert response.get_json()["admin"]
-    response = client.post("/admin/status/", json={"username": "Korneel"})
-    assert response.status_code == HTTPStatus.OK
-    assert not response.get_json()["admin"]
 
 
 def test_add_admin_no_json(client: Flask.testing):
@@ -60,44 +50,6 @@ def test_add_admin_non_existing_user(client: Flask.testing):
     expected = """{
         "error": "RecordNotFoundError",
         "message": "An operation failed because it depends on one or more records that were required but not found. Record to update not found."
-    }"""
-    assert response.status_code == HTTPStatus.BAD_REQUEST
-    assert compare_json(expected, response.get_json())
-
-
-# Status
-def test_status(client: Flask.testing):
-    response = client.post("/user/", json={"username": "jan", "password": "qwerty"})
-    assert response.status_code == HTTPStatus.OK
-    response = client.post("/user/", json={"username": "piet-joris", "password": "qwerty"})
-    assert response.status_code == HTTPStatus.OK
-
-    response_make_admin = client.post("/admin/", json={"usernames": ["jan"]})
-    assert response_make_admin.status_code == HTTPStatus.OK
-
-    response = client.post("/admin/status/", json={"username": "Jan"})
-    assert response.status_code == HTTPStatus.OK
-    assert response.get_json()["admin"]
-    response = client.post("/admin/status/", json={"username": "piet-joris"})
-    assert response.status_code == HTTPStatus.OK
-    assert not response.get_json()["admin"]
-
-
-def test_status_no_json(client: Flask.testing):
-    response = client.post("/admin/status/")
-    expected = """{
-        "error": "InvalidJson",
-        "message": ""
-    }"""
-    assert response.status_code == HTTPStatus.BAD_REQUEST
-    assert compare_json(expected, response.get_json())
-
-
-def test_status_schema(client: Flask.testing):
-    response = client.post("/admin/status/", json={"test": "test"})
-    expected = """{
-        "error": "JsonValidationError",
-        "message": "'username' is a required property"
     }"""
     assert response.status_code == HTTPStatus.BAD_REQUEST
     assert compare_json(expected, response.get_json())
