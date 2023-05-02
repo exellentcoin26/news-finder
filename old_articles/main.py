9bf41b018b99e2a3252257af018cb24671c7418a
Ayoub El Mar <95707801+ayoubelmarch@users.noreply.github.com>
2023-05-02 20:52:05 +0200

    89 delete old articles (#70)
    * delete old articles
    
    * delete old articles
    
    * temporary old_articles folder
    
    * almost done
    
    * delet old articles done
    
    * done
    
    ---------
    
    Co-authored-by: Ayoub Elmar <ayoub@Ayoubs-MacBook-Air.local>
    Co-authored-by: Laurens <93316351+Laurensdewachter@users.noreply.github.com>

---
+++ old_articles/main.py
@@ -0,0 +1,63 @@
+import asyncio
+
+import sys
+from datetime import date
+
+
+import schedule
+from prisma import Prisma
+
+
+# failsafe for running async call with non async scheduler.
+# Note: this fails if the schedule timestep is lower than the starting of the thread
+# (which _should_ never happen).
+is_running = False
+
+
+async def main():
+    schedule.every(24*60).minutes.do(run)  # pyright: ignore
+
+    while True:
+        schedule.run_pending()
+        next_run = schedule.next_run()
+        if next_run is None:
+            exit(0)
+
+        delta = next_run - date.now()
+
+        print(
+            f"Time until next run: {int(delta.total_seconds())} seconds",
+            file=sys.stderr,
+        )
+        await asyncio.sleep(3)
+
+
+def run():
+    asyncio.get_event_loop().create_task(delete_old_articles())
+
+
+def delete_old_articles():
+    global is_running
+
+    if is_running:
+        print("Not running checker because last run has not finished", file=sys.stderr)
+        return
+
+    is_running = True
+
+    db = Prisma()
+    await db.connect()
+    articles = await db.newsarticles.find_many()
+    today = date.today()
+
+    for article in articles:
+        delta_article = today - article.publication_date
+        if delta_article.days > 2:
+            db.newsarticles.delete(where={"id": article.id})
+
+    await db.disconnect()
+    
+    is_running = False
+
+if __name__ == "__main__":
+    asyncio.run(main())
