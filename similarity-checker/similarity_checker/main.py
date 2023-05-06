e9187e2f64b6a991a008119d7e9e15cd344bf6bb
exellentcoin26 <jonas.caluwe26@gmail.com>
2023-05-06 16:27:16 +0200

    Speedup similarity checker by 693x

--- similarity-checker/similarity_checker/main.py
+++ similarity-checker/similarity_checker/main.py
@@ -14,6 +14,9 @@ import schedule
 from prisma.models import NewsArticles
 from prisma import Prisma
 
+import numpy as np
+from numpy.typing import NDArray
+
 from utils import Language, filter_stop_words, filter_numerics
 
 THRESHOLD = 0.50
@@ -27,7 +30,7 @@ is_running = False
 
 
 async def main():
-    schedule.every(30).minutes.do(run)  # pyright: ignore
+    schedule.every(2).seconds.do(run)  # type: ignore
 
     while True:
         schedule.run_pending()
@@ -106,23 +109,19 @@ async def calc_article_similarity(
 
     similar: Dict[int, Set[int]] = {}
 
+    cosine_similarity_table = np.matmul(tf_idf_table, tf_idf_table.T)
+
     # Check every article against every other article
     print("Checking cosine similarities...", file=sys.stderr)
     for lhs_article_idx in range(len(articles)):
         for rhs_article_idx in range(lhs_article_idx + 1, len(articles)):
-            # Only compare articles from different sources
             article1, article2 = (
                 databse_articles[lhs_article_idx],
                 databse_articles[rhs_article_idx],
             )
             assert article1.source is not None and article2.source is not None
-            # if article1.source.id == article2.source.id:
-            #     continue
 
-            similarity = cos_similarity(
-                extract_tf_idf_values(tf_idf_table, lhs_article_idx),
-                extract_tf_idf_values(tf_idf_table, rhs_article_idx),
-            )
+            similarity = cosine_similarity_table[lhs_article_idx][rhs_article_idx]
             sys.stdout.write(
                 "\033[K"
                 + f"Article `{lhs_article_idx}` == Article `{rhs_article_idx}`: {similarity}".expandtabs(
@@ -205,48 +204,50 @@ async def insert_similar_article_pair_into_db(
     )
 
 
-def calc_tf_idf(documents: List[List[str]]) -> Dict[str, Tuple[List[float], float]]:
-    # Join all frequency values in a single table
-    tf_table: Dict[str, List[float]] = {}
-    for [doc_idx, document] in enumerate(documents):
-        tf = calc_norm_term_freq(document)
+def calc_tf_idf(
+    documents: List[List[str]],
+) -> NDArray[np.float32]:
+    words: NDArray[np.str_] = np.unique(  # type: ignore
+        [word for document in documents for word in document]
+    )
+    words.sort()
 
-        for [term, freq] in tf.items():
-            # Insert if term already in table. Create new list with empty entries up
-            # until current idx.
-            if term not in tf_table:
-                # Insert 0 entries
-                tf_table[term] = [0 for _ in range(doc_idx)]
+    # Term frequencies per document
+    tf_table = np.zeros([len(documents), len(words)])
 
-            tf_table[term].append(freq)
+    for [i, document] in enumerate(documents):
+        document_tf_array = calc_norm_term_freq(document, words)
+        tf_table[i] = document_tf_array
 
-        # Add 0 entry for terms present in table but not in current document
-        for term in tf_table.keys():
-            if term not in tf:
-                tf_table[term].append(0)
+    # Amount of documents containing the word
+    df_counts = np.count_nonzero(tf_table, axis=0)  # type: ignore
 
-    idf_table = calc_idf(list(tf_table.keys()), documents)
-    tf_idf_table = {
-        term: (freq, idf_table.get(term, 0)) for [term, freq] in tf_table.items()
-    }
+    idf_counts = len(words) / df_counts
 
-    return tf_idf_table
+    # Muliply every row element-wise with the idf_counts col vector
+    tf_table *= idf_counts.T
 
-
-def calc_norm_term_freq(document: List[str]) -> Dict[str, float]:
-    tf_table: Dict[str, float] = {}
-    total_count = 0
-
-    # Count terms occurrences in document
-    for term in document:
-        tf_table[term] = tf_table.get(term, 0) + 1
-        total_count += 1
-
-    # Normalize counts
-    for term in tf_table.keys():
-        tf_table[term] /= total_count
+    # Normalize rows
+    tf_table /= np.linalg.norm(tf_table, axis=1, keepdims=True)  # type: ignore
 
     return tf_table
+
+
+def calc_norm_term_freq(
+    document: List[str], words: NDArray[np.str_]
+) -> NDArray[np.float32]:
+    tf_array = np.zeros(len(words), dtype=float)
+
+    # Count terms occurrences in document
+    unique_words, word_count = np.unique(document, return_counts=True)  # type: ignore
+    for [i, word] in enumerate(unique_words):
+        idx = np.where(words == word)[0][0]  # type: ignore
+        tf_array[idx] = word_count[i]
+
+    # Normalize counts
+    tf_array /= np.sum(tf_array)  # type: ignore
+
+    return tf_array  # type: ignore
 
 
 def calc_idf(terms: List[str], documents: List[List[str]]) -> Dict[str, float]:
