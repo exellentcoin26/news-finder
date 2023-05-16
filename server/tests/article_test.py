641c5f1066893fbde1408cf96001e8318da695e6
chloeacm <anne.mansibang@hotmail.com>
2023-05-16 14:18:05 +0200

    test similar articles

--- server/tests/article_test.py
+++ server/tests/article_test.py
@@ -50,3 +50,49 @@ async def test_get_articles_with_updated_version(client: Flask.testing):
 
     loop = asyncio.get_running_loop()
     await loop.run_in_executor(None, sync_part)
+
+
+async def test_get_similar_articles(client: Flask.testing):
+    db = Prisma()
+    await db.connect()
+
+    source = await db.newssources.create(
+        data={"url": "https://test.com", "name": "test.com"}
+    )
+
+    article1 = await db.newsarticles.create(
+        data={
+            "source": {"connect": {"id": source.id}},
+            "url": "https://example.com/article1",
+            "title": "Article 1",
+            "publication_date": datetime.now() - timedelta(hours=4),
+        }
+    )
+    article2 = await db.newsarticles.create(
+        data={
+            "source": {"connect": {"id": source.id}},
+            "url": "https://example.com/article2",
+            "title": "Article 2",
+            "publication_date": datetime.now(),
+        }
+    )
+
+    await db.similararticles.create(
+        data={"id1": article1.id, "id2": article2.id, "similarity": 0.9}
+    )
+    await db.similararticles.create(
+        data={"id1": article2.id, "id2": article1.id, "similarity": 0.9}
+    )
+
+    await db.disconnect()
+
+    def sync_part():
+        response = client.get("/article/similar?url=https://example.com/article1")
+        assert response.status_code == HTTPStatus.OK
+        articles = response.get_json()["data"]["articles"]
+
+        assert len(articles) == 1
+        assert articles[0]["link"] == "https://example.com/article2"
+        
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None, sync_part)
